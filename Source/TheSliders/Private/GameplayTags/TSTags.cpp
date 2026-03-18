68ff4ba147a9a42a3e68cefc3907a05c578b60dc
Alex <112611942+bsswd@users.noreply.github.com>
2026-03-18 23:52:55 +0700

    Add NativeTags file

---
+++ Source/TheSliders/Private/GameplayTags/TSTags.cpp
@@ -0,0 +1,13 @@
+﻿// The Sliders Game. Made by Alex Sinkin (C)
+
+
+#include "GameplayTags/TSTags.h"
+
+
+namespace TSTags
+{
+	namespace TSAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(MeleeAttack, "TSTags.TSAbilities.MeleeAttack", "Tag for melee attack")
+	}
+}
---
+++ Source/TheSliders/Public/GameplayTags/TSTags.h
@@ -0,0 +1,16 @@
+﻿// The Sliders Game. Made by Alex Sinkin (C)
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+
+namespace TSTags
+{
+	namespace TSAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(MeleeAttack);
+	}
+}
