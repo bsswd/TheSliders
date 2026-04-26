b88b8011bcc9bc31a0ad9682c67b9f1f78e55dbd
Alex <112611942+bsswd@users.noreply.github.com>
2026-04-26 11:58:48 +0700

    need to check frame index

--- Source/TheSliders/Private/Components/AttackComponent.cpp
+++ Source/TheSliders/Private/Components/AttackComponent.cpp
@@ -2,13 +2,31 @@
 
 
 #include "Components/AttackComponent.h"
+#include "Engine/Engine.h"
+#include "PaperFlipbookComponent.h"
+#include "PaperFlipbook.h"
+#include "GameFramework/Actor.h"
 #include "Characters/TS_BaseCharacter.h"
 #include "Engine/World.h"
+#include "TimerManager.h"
 
 
 UAttackComponent::UAttackComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UAttackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorld()->GetTimerManager().SetTimer(
+		CheckAttackFrameTimerHandle,
+		this,
+		&ThisClass::CheckAttackFrameIndex,
+		0.1f,
+		true
+	);
 }
 
 void UAttackComponent::PerformAttack()
@@ -63,5 +81,38 @@ FVector UAttackComponent::GetCalculatedDirection() const
 	}
 	
 	return FVector(-1.0f, 0.0f, 0.0f);
+}
+
+void UAttackComponent::CheckAttackFrameIndex()
+{
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		UE_LOG(LogAttackComponent, Error, TEXT("Owner not valid!"))
+		return;
+	}
+ 
+	UPaperFlipbookComponent* Sprite = Owner->FindComponentByClass<UPaperFlipbookComponent>();
 	
+	if (!Sprite && !Sprite->GetFlipbook())
+	{
+		UE_LOG(LogAttackComponent, Error, TEXT("Sprite and Flipbook not valid!"))
+		return;
+	}
+	
+	const int32 FrameIndex = Sprite->GetPlaybackPositionInFrames();
+	const FString FlipbookName = Sprite->GetFlipbook()->GetName();
+		
+	if (!FlipbookName.Contains(TEXT("Attack")))
+	{
+		return;
+	}
+ 
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+				1, 1.1f, FColor::Cyan, 
+				FString::Printf(TEXT("Flipbook: %s | Frame: %d"), *FlipbookName, FrameIndex)
+			);
+	}
 }
--- Source/TheSliders/Public/Components/AttackComponent.h
+++ Source/TheSliders/Public/Components/AttackComponent.h
@@ -8,6 +8,9 @@
 
 #define COLLISION_ENEMY ECC_GameTraceChannel1
 
+DEFINE_LOG_CATEGORY_STATIC(LogAttackComponent, Log, All)
+
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class THESLIDERS_API UAttackComponent : public UActorComponent
 {
@@ -31,10 +34,28 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TS|Attack|Debug")
 	bool bDrawDebug = true;
 	
+	FTimerHandle CheckAttackFrameTimerHandle;
+	
+	
+	/** FUNCTIONS **/
+	
 	UFUNCTION(BlueprintCallable, Category = "Attack")
 	void PerformAttack();
  
+	
 protected:
 	
+	/** PROPERTIES **/
+	
 	FVector GetCalculatedDirection() const;	
+	
+	
+	/** FUNCTIONS **/
+	
+	virtual void BeginPlay() override;
+	
+	
+private:
+	
+	void CheckAttackFrameIndex();
 };
